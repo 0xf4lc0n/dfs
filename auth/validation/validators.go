9cc3eb43fef4ed763ae7a4d27cb5448371841357
0xf4lc0n <pmakowiecki0@gmail.com>
2022-03-30 20:13:53 +0200

    feat: add password complexity validator

--- auth/validation/validators.go
+++ auth/validation/validators.go
@@ -2,11 +2,13 @@ package validation
 
 import (
 	"auth/dtos"
+	"unicode"
 
 	"github.com/go-playground/validator"
 )
 
 var validate = validator.New()
+var _ = validate.RegisterValidation("password", validatePasswordComplexity)
 
 func Validate[V dtos.LoginDto | dtos.RegisterDto](v *V) []string {
 	var invalidFields []string
@@ -19,4 +21,38 @@ func Validate[V dtos.LoginDto | dtos.RegisterDto](v *V) []string {
 	}
 
 	return invalidFields
+}
+
+func validatePasswordComplexity(fl validator.FieldLevel) bool {
+	password := fl.Field().String()
+	var number bool
+	var bigLetter bool
+	var smallLetter bool
+	var specialCharacter bool
+	characters := make(map[rune]int)
+
+	if len(password) < 12 || len(password) > 48 {
+		return false
+	}
+
+	for _, c := range password {
+		switch {
+		case unicode.IsNumber(c):
+			number = true
+		case unicode.IsUpper(c):
+			bigLetter = true
+			characters[c] += 1
+		case unicode.IsLower(c):
+			smallLetter = true
+			characters[c] += 1
+		case unicode.IsSymbol(c) || unicode.IsPunct(c):
+			specialCharacter = true
+		}
+	}
+
+	if len(characters) < 6 {
+		return false
+	}
+
+	return number && bigLetter && smallLetter && specialCharacter
 }
