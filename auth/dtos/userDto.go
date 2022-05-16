3ec4f52bfd981ca2c65e1cdeda1bce966baefaa4
0xf4lc0n <pmakowiecki0@gmail.com>
2022-05-16 23:02:25 +0200

    feat: create user dto

---
+++ auth/dtos/userDto.go
@@ -0,0 +1,13 @@
+package dtos
+
+import "dfs/auth/models"
+
+type User struct {
+	Id            uint          `json:"id"`
+	Name          string        `json:"name"`
+	Email         string        `json:"email" gorm:"unique"`
+	Verified      bool          `json:"verified"`
+	HomeDirectory string        `json:"directory"`
+	CryptKey      string        `json:"cryptKey"`
+	OwnedFiles    []models.File `json:"ownedFiles"`
+}
