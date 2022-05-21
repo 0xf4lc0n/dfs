880260534f666955bc19a08a66492edf35de0223
0xf4lc0n <pmakowiecki0@gmail.com>
2022-05-21 21:49:37 +0200

    feat: add script to run required docker containers

---
+++ RunContainers.ps1
@@ -0,0 +1,11 @@
+Write-Host "Running postgres database for Auth microservice"
+docker run -d --rm -p 5432:5432 -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=dfs_auth" --name pg_auth postgres:latest
+
+Write-Host "Running postgres database for Storage microservice"
+docker run -d --rm -p 5433:5432 -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=dfs_storage" --name pg_storage postgres:latest
+
+Write-Host "Running postgres database for Share microservice"
+docker run -d --rm -p 5434:5432 -e "POSTGRES_PASSWORD=postgres" -e "POSTGRES_DB=dfs_share" --name pg_share postgres:latest
+
+Write-Host "Running RabbitMq"
+docker run -d --rm -p 5672:5672 -p 15672:15672 --name rabbitmq rabbitmq:3.10-management
