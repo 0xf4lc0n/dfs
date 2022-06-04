b33d56e860ae4a42ccd8ea150d8cc31e120d8fc0
0xf4lc0n <pmakowiecki0@gmail.com>
2022-06-04 13:32:53 +0200

    refactor(Share): use connection string from config in database connection

--- share/database/connection.go
+++ share/database/connection.go
@@ -3,21 +3,11 @@ package database
 import (
 	"dfs/share/models"
 	"errors"
-	"os"
-
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func Connect() (*gorm.DB, error) {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		return nil, errors.New("Cannot load .env file")
-	}
-
-	connectionString := os.Getenv("DB_CONNECTION_STRING")
+func Connect(connectionString string) (*gorm.DB, error) {
 	connection, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 
 	if err != nil {
